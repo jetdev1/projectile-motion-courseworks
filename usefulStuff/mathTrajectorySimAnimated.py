7bc3c05377b6be70fdf1b1597b2d6d53324dd8b9
jetdev1 <limjit30@gmail.com>
2021-08-05 16:46:36 +0800

    final commit

--- usefulStuff/mathTrajectorySimAnimated.py
+++ usefulStuff/mathTrajectorySimAnimated.py
@@ -17,23 +17,31 @@ class Shot:
         self.yCoordinates = [2.1]
 
     def animate(self, time):
-        t = time / timeInterval
-        self.xDisplacement = self.currentVelX * t
-        self.x = 0 + self.xDisplacement
-        self.xCoordinates.append(self.x)
-        line.set_xdata(self.xCoordinates)
+        if self.y > 0:
+            t = time / timeInterval
+            self.xDisplacement = self.currentVelX * t
+            self.x = 0 + self.xDisplacement
+            self.xCoordinates.append(self.x)
+            line.set_xdata(self.xCoordinates)
 
-        self.yDisplacement = self.currentVelY * t + (self.gravityAccel * t**2)/2
-        self.y = 2.1 + self.yDisplacement
-        self.yCoordinates.append(self.y)
-        line.set_ydata(self.yCoordinates)
-        return line,
+            self.yDisplacement = self.currentVelY * t + (self.gravityAccel * t**2)/2
+            self.y = 2.1 + self.yDisplacement
+            self.yCoordinates.append(self.y)
+            line.set_ydata(self.yCoordinates)
+            return line,
+
+        else:
+            return line,
 
 def main():
     global line
     global x
     global timeInterval
-    timeInterval = 500
+    global angleInterval
+
+    timeInterval = 1000
+    angleInterval = 5
+    angleList = ["42"+ "°"]
     fig, ax = plt.subplots()
 
     shot = Shot(42)
@@ -43,9 +51,14 @@ def main():
     ani = animation.FuncAnimation(
     fig, shot.animate, interval=1, blit=True, save_count=50
     )
+
+    #Create the legend for the graphs
+    plt.legend(angleList, loc="upper right")
+    plt.subplots_adjust(left=0.095, bottom=0.105, right=0.974, top=0.97, wspace=None, hspace=None)
+
+    plt.xlabel("horizontal distance travelled by projectile")
+    plt.ylabel("height of projectile")
     plt.show()
-
-
 
 
 if __name__=="__main__":
